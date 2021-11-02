81c097dbbdb919892f5592000c4eb2e9f8489ad2
goosemansb <sander@socialbrothers.nl>
2021-11-02 17:39:56 +0100

    added Products header

---
+++ templates/blocks/products.php
@@ -0,0 +1,20 @@
+<div class="products">
+    <img src="./assets/images/product-background.svg" class="products__background-img" alt="product background figure">
+    
+    <div class="products__header">
+        <div class="products__alphabetically">
+            <span id="sort_alphabetically">Sort Alphabetically</span>
+        </div>
+        <div class="products__title-wrapper">
+            <img src="./assets/images/canada-flag.svg" class="products__c-flag" alt="canada flag">
+            <h1 class="products__title">BEST SPORTS BETTING SITES</h1>
+        </div>
+        <div class="products__change-view">
+            <div class="custom-switch products__switch">
+                <input type="checkbox" class="custom-control-input" id="viewSwitch">
+                <label class="custom-control-label" for="viewSwitch">Change view</label>
+            </div>
+        </div>
+    </div>
+</div>
+
