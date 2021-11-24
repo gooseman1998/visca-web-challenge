db33e6cdf31e4ffd6ff30bfd2feb42be471cd21a
goosemansb <sander@socialbrothers.nl>
2021-11-24 02:24:15 +0100

    Changed block structure to be completely clickable

--- templates/blocks/products.php
+++ templates/blocks/products.php
@@ -20,10 +20,13 @@
     <div class="row product-cards product-cards--list">
         
         <div class="col product-card product-card--list" id="sportsInteractionCard">
-            <span class="product-card__number">
-                1
-            </span>
-            <img src="./assets/images/logos/sport-interaction-logo.svg" alt="product logo" class="product-card__logo">
+            <a href="#partner" class="product-card__link"></a>
+            <span class="product-card__number">1</span>
+            <div class="product-card__logo-wrapper">
+                <a href="#partnerReviewImg" class="product-card__logo-link">
+                    <img src="./assets/images/logos/sport-interaction-logo.svg" alt="product logo" class="product-card__logo">
+                </a>
+            </div>
             <div class="product-card__review-wrapper">
                 <div class="product-card__stars">
                     <img src="./assets/images/misc/star.svg" class="product-card__star">
@@ -32,20 +35,22 @@
                     <img src="./assets/images/misc/star.svg" class="product-card__star">
                     <img src="./assets/images/misc/star.svg" class="product-card__star">
                 </div>
-                <a href="#" class="product-card__show-review product-card__show-review--desktop">Read review</a>
-                <a href="#" class="product-card__show-review product-card__show-review--mobile">Review</a>
+                <a href="#partnerReview" class="product-card__show-review product-card__show-review--desktop">Read review</a>
+                <a href="#partnerReview" class="product-card__show-review product-card__show-review--mobile">Review</a>
             </div>
             <div class="product-card__sale-offer product-card__sale-offer--desktop"><span class="product-card__line-break"> <span class="product-card__sale-offer--bold"> - Exclusive - </span></span> Bonus up to <strong>$200</strong></div>
             <div class="product-card__sale-offer product-card__sale-offer--mobile"> <span class="product-card__line-break"> BONUS </span> <strong>$200</strong></div>
-            <a href="#"  class="btn btn-primary product-card__button product-card__button--desktop">Play Now <img src="./assets/images/misc/chevron.svg" class="product-card__button__chevron"></a>
-            <a href="#"  class="btn btn-primary product-card__button product-card__button--mobile"><img src="./assets/images/misc/chevron.svg" class="product-card__button__chevron product-card__button__chevron--mobile"></a>
-
+            <div class="btn btn-primary product-card__button product-card__button--desktop">Play Now <img src="./assets/images/misc/chevron.svg" class="product-card__button__chevron"></div>
+            <div class="btn btn-primary product-card__button product-card__button--mobile"><img src="./assets/images/misc/chevron.svg" class="product-card__button__chevron product-card__button__chevron--mobile"></div>
         </div>
         <div class="col product-card product-card--list" id="powerPlaySportsCard">
-            <span class="product-card__number">
-                2
-            </span>
-            <img src="./assets/images/logos/powerplay-logo.png" alt="product logo" class="product-card__logo">
+            <a href="#partner" class="product-card__link"></a>
+            <span class="product-card__number">2</span>
+            <div class="product-card__logo-wrapper">
+                <a href="#partnerReviewImg" class="product-card__logo-link">
+                    <img src="./assets/images/logos/powerplay-logo.png" alt="product logo" class="product-card__logo">
+                </a>
+            </div>
             <div class="product-card__review-wrapper">
                 <div class="product-card__stars">
                     <img src="./assets/images/misc/star.svg" class="product-card__star">
@@ -54,20 +59,23 @@
                     <img src="./assets/images/misc/star.svg" class="product-card__star">
                     <img src="./assets/images/misc/star.svg" class="product-card__star">
                 </div>
-                <a href="#" class="product-card__show-review product-card__show-review--desktop">Read review</a>
-                <a href="#" class="product-card__show-review product-card__show-review--mobile">Review</a>
+                <a href="#partnerReview" class="product-card__show-review product-card__show-review--desktop">Read review</a>
+                <a href="#partnerReview" class="product-card__show-review product-card__show-review--mobile">Review</a>
             </div>
             <div class="product-card__sale-offer product-card__sale-offer--desktop"><span class="product-card__line-break"><span class="product-card__sale-offer--bold">- Exclusive - </span></span> Bonus up to <strong>$200</strong></div>
             <div class="product-card__sale-offer product-card__sale-offer--mobile"> <span class="product-card__line-break"> FREE BET </span> <strong>$250</strong></div>
 
-            <a href="#"  class="btn btn-primary product-card__button product-card__button--desktop">Play Now <img src="./assets/images/misc/chevron.svg" class="product-card__button__chevron"></a>
-            <a href="#"  class="btn btn-primary product-card__button product-card__button--mobile"><img src="./assets/images/misc/chevron.svg" class="product-card__button__chevron product-card__button__chevron--mobile"></a>
+            <div class="btn btn-primary product-card__button product-card__button--desktop">Play Now <img src="./assets/images/misc/chevron.svg" class="product-card__button__chevron"></div>
+            <div class="btn btn-primary product-card__button product-card__button--mobile"><img src="./assets/images/misc/chevron.svg" class="product-card__button__chevron product-card__button__chevron--mobile"></div>
         </div>
         <div class="col product-card product-card--list" id="betwayCard">
-            <span class="product-card__number">
-                3
-            </span>
-            <img src="./assets/images/logos/betway-logo.png" alt="product logo" class="product-card__logo">
+            <a href="#partner" class="product-card__link"></a>
+            <span class="product-card__number">3</span>
+            <div class="product-card__logo-wrapper">
+                <a href="#partnerReviewImg" class="product-card__logo-link">
+                    <img src="./assets/images/logos/betway-logo.png" alt="product logo" class="product-card__logo">
+                </a>
+            </div>
             <div class="product-card__review-wrapper">
                 <div class="product-card__stars">
                     <img src="./assets/images/misc/star.svg" class="product-card__star">
@@ -76,20 +84,23 @@
                     <img src="./assets/images/misc/star.svg" class="product-card__star">
                     <img src="./assets/images/misc/star.svg" class="product-card__star">
                 </div>
-                <a href="#" class="product-card__show-review product-card__show-review--desktop">Read review</a>
-                <a href="#" class="product-card__show-review product-card__show-review--mobile">Review</a>
+                <a href="#partnerReview" class="product-card__show-review product-card__show-review--desktop">Read review</a>
+                <a href="#partnerReview" class="product-card__show-review product-card__show-review--mobile">Review</a>
             </div>
             <div class="product-card__sale-offer product-card__sale-offer--desktop"><span class="product-card__line-break">100% Sign Up Bonus</span> Up to <strong>$200</strong></div>
             <div class="product-card__sale-offer product-card__sale-offer--mobile"> <span class="product-card__line-break"> BONUS </span> <strong>$250</strong></div>
 
-            <a href="#"  class="btn btn-primary product-card__button product-card__button--desktop">Play Now <img src="./assets/images/misc/chevron.svg" class="product-card__button__chevron"></a>
-            <a href="#"  class="btn btn-primary product-card__button product-card__button--mobile"><img src="./assets/images/misc/chevron.svg" class="product-card__button__chevron product-card__button__chevron--mobile"></a>
+            <div class="btn btn-primary product-card__button product-card__button--desktop">Play Now <img src="./assets/images/misc/chevron.svg" class="product-card__button__chevron"></div>
+            <div class="btn btn-primary product-card__button product-card__button--mobile"><img src="./assets/images/misc/chevron.svg" class="product-card__button__chevron product-card__button__chevron--mobile"></div>
         </div>
         <div class="col product-card product-card--list" id="spinSportsCard">
-            <span class="product-card__number">
-                4
-            </span>
-            <img src="./assets/images/logos/spinsports-logo.svg" alt="product logo" class="product-card__logo">
+            <a href="#partner" class="product-card__link"></a>
+            <span class="product-card__number">4</span>
+            <div class="product-card__logo-wrapper">
+                <a href="#partnerReviewImg" class="product-card__logo-link">
+                    <img src="./assets/images/logos/spinsports-logo.svg" alt="product logo" class="product-card__logo">
+                </a>
+            </div>
             <div class="product-card__review-wrapper">
                 <div class="product-card__stars">
                     <img src="./assets/images/misc/star.svg" class="product-card__star">
@@ -98,20 +109,23 @@
                     <img src="./assets/images/misc/star.svg" class="product-card__star">
                     <img src="./assets/images/misc/star.svg" class="product-card__star">
                 </div>
-                <a href="#" class="product-card__show-review product-card__show-review--desktop">Read review</a>
-                <a href="#" class="product-card__show-review product-card__show-review--mobile">Review</a>
+                <a href="#partnerReview" class="product-card__show-review product-card__show-review--desktop">Read review</a>
+                <a href="#partnerReview" class="product-card__show-review product-card__show-review--mobile">Review</a>
             </div>
             <div class="product-card__sale-offer product-card__sale-offer--desktop"><span class="product-card__line-break"><span class="product-card__sale-offer--bold"> - Exclusive - </span></span>Up to <strong>$500</strong> Free Bet</div>
             <div class="product-card__sale-offer product-card__sale-offer--mobile"> <span class="product-card__line-break"> FREE BET </span> <strong>$200</strong></div>
 
-            <a href="#"  class="btn btn-primary product-card__button product-card__button--desktop">Play Now <img src="./assets/images/misc/chevron.svg" class="product-card__button__chevron"></a>
-            <a href="#"  class="btn btn-primary product-card__button product-card__button--mobile"><img src="./assets/images/misc/chevron.svg" class="product-card__button__chevron product-card__button__chevron--mobile"></a>
+            <div class="btn btn-primary product-card__button product-card__button--desktop">Play Now <img src="./assets/images/misc/chevron.svg" class="product-card__button__chevron"></div>
+            <div class="btn btn-primary product-card__button product-card__button--mobile"><img src="./assets/images/misc/chevron.svg" class="product-card__button__chevron product-card__button__chevron--mobile"></div>
         </div>
         <div class="col product-card product-card--list"  id="bodogCard">
-            <span class="product-card__number">
-                5
-            </span>
-            <img src="./assets/images/logos/bodog-logo.svg" alt="product logo" class="product-card__logo">
+            <a href="#partner" class="product-card__link"></a>
+            <span class="product-card__number">5</span>
+            <div class="product-card__logo-wrapper">
+                <a href="#partnerReviewImg" class="product-card__logo-link">
+                    <img src="./assets/images/logos/bodog-logo.svg" alt="product logo" class="product-card__logo">
+                </a>
+            </div>
             <div class="product-card__review-wrapper">
                 <div class="product-card__stars">
                     <img src="./assets/images/misc/star.svg" class="product-card__star">
@@ -120,18 +134,18 @@
                     <img src="./assets/images/misc/star.svg" class="product-card__star">
                     <img src="./assets/images/misc/star.svg" class="product-card__star">
                 </div>
-                <a href="#" class="product-card__show-review product-card__show-review--desktop">Read review</a>
-                <a href="#" class="product-card__show-review product-card__show-review--mobile">Review</a>
+                <a href="#partnerReview" class="product-card__show-review product-card__show-review--desktop">Read review</a>
+                <a href="#partnerReview" class="product-card__show-review product-card__show-review--mobile">Review</a>
             </div>
             <div class="product-card__sale-offer product-card__sale-offer--desktop"><span class="product-card__line-break">100% Sign Up Bonus</span> Up to <strong>$200</strong></div>
             <div class="product-card__sale-offer product-card__sale-offer--mobile"> <span class="product-card__line-break"> BONUS </span> <strong>$200</strong></div>
 
-            <a href="#"  class="btn btn-primary product-card__button product-card__button--desktop">Play Now <img src="./assets/images/misc/chevron.svg" class="product-card__button__chevron"></a>
-            <a href="#"  class="btn btn-primary product-card__button product-card__button--mobile"><img src="./assets/images/misc/chevron.svg" class="product-card__button__chevron product-card__button__chevron--mobile"></a>
+            <div class="btn btn-primary product-card__button product-card__button--desktop">Play Now <img src="./assets/images/misc/chevron.svg" class="product-card__button__chevron"></div>
+            <div class="btn btn-primary product-card__button product-card__button--mobile"><img src="./assets/images/misc/chevron.svg" class="product-card__button__chevron product-card__button__chevron--mobile"></div>
         </div>
     </div>
     <div class="product-cards__view-more">
-        <a href="#" class="product-cards__view-more-link">
+        <a href="#partnerReview" class="product-cards__view-more-link">
             View All Sports Betting Sites
         </a>
     </div>
